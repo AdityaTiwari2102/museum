54852d1a4447e6380ee733c56e59f869a26b6467
ADITYA TIWARI <taditya.tiwari007@gmail.com>
2020-01-15 17:54:28 +0530

    refactor: new routes and pages created

--- app/controllers/museum_controller.rb
+++ app/controllers/museum_controller.rb
@@ -1,4 +1,22 @@
 class MuseumController < ApplicationController
   def index
   end
+
+  def foundation
+  end
+
+  def first_of_journey
+  end
+
+  def founder
+  end
+
+  def initial_board_members
+  end
+
+  def pioneer
+  end
+
+  def celebration
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  get 'museum/index'
+  get 'foundation', to: 'museum#foundation'
+  get 'first_of_journey', to: 'museum#first_of_journey'
+  get 'founder', to: 'museum#founder'
+  get 'initial_board_members', to: 'museum#initial_board_members'
+  get 'pioneer', to: 'museum#pioneer'
+  get 'celebration', to: 'museum#celebration'
+
   root to: 'museum#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
